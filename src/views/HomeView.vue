907d48137e0eaa1b2a64bdb4aa409b98643d3f59
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-11-26 15:08:50 +0800

    ccText是footer，在每個頁下方都呈現, close #10

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,24 +43,6 @@ useHead({
       </div>
     </div>
   </div>
-  <footer class="full-page__row">
-    <div class="full-page__unit" style="display: flex">
-      <div class="row">
-        <div class="columns small-12 large-9" style="padding: 0">
-          <p style="font-size: 1em; font-weight: bold; display: flex; margin-inline-end: auto;" id="cc">
-            本站由 唐鳳 維運，除另有標示外，內容以創用 CC0 授權條款釋出<br>
-            This site is maintained and operated by Audrey Tang. Unless otherwise indicated, the content is released under the terms of the Creative Commons CC0 license.
-          </p>
-        </div>
-        <div class="columns small-12 large-3" style="padding: 0">
-          <p style="font-size: 1em; font-weight: bold; display: flex; justify-content: end">
-            <a id="tos"></a>
-            <a id="privacy" style="margin-left: 1rem"></a>
-          </p>
-        </div>
-      </div>
-    </div>
-  </footer>
 </template>
 
 <style scoped></style>
